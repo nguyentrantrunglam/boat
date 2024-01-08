cc212f747954822805d342ca68c16c37676815eb
trunglam <nguyentrantrunglam@gmail.com>
2024-01-08 23:55:45 +0700

    render header-nav

---
+++ index.js
@@ -0,0 +1,50 @@
+var activeHeader2Id = 3
+
+const headerNavItems = [
+    {
+        id: 1,
+        label: "Home",
+        img: "home"
+    },
+    {
+        id: 2,
+        label: "Post",
+        img: "paper-plane"
+    }
+    , {
+        id: 3,
+        label: "Activity",
+        img: "heart"
+    }
+    , {
+        id: 4,
+        label: "Bookmark",
+        img: "bookmark"
+    }
+]
+
+function renderHeader2Item(item, isActive) {
+    return `
+    <div class="item" onclick="handleClickHeader2Item(${item.id})">
+        <div class="icon"><img src="./assets/${item.img}-alt${isActive ? "-active" : ''}.svg" alt=""></div>
+        <div class="${isActive ? "htitle-color" : "htitle"}">${item.label}</div>
+    </div>
+    `
+}
+
+function renderHeader2() {
+    const header2 = document.querySelector(".header-2")
+    let innerHeader2 = ''
+    headerNavItems.forEach((item) => {
+        innerHeader2 += renderHeader2Item(item, item.id == activeHeader2Id)
+    })
+    header2.innerHTML = innerHeader2
+
+
+}
+function handleClickHeader2Item(id) {
+    activeHeader2Id = id //set active id
+    renderHeader2()
+}
+
+renderHeader2()
